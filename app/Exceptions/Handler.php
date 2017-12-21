c7e264b77d46b2fa3592ad93d3b871446693d91c
Julien Bourdeau <julien@sigerr.org>
2017-12-21 15:26:58 +0100

    Change Exception Handler to avoid html response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,17 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\Debug\Exception\FlattenException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +51,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        if (env('APP_DEBUG')) {
+            return parent::render($request, $e);
+        }
+
+        if ($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        } elseif ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        } elseif ($e instanceof AuthorizationException) {
+            $e = new HttpException(403, $e->getMessage());
+        } elseif ($e instanceof ValidationException && $e->getResponse()) {
+            return $e->getResponse();
+        }
+
+        $content  = [
+            'message' => $e->getMessage() ?: 'Oops',
+        ];
+
+        $response = new Response($content, $e->getStatusCode(), $e->getHeaders());
+
+        $response->exception = $e;
+
+        return $response;
     }
 }
