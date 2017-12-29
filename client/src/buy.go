c45cbd9fa4259adb5a3a0333bafdb663f45ab571
Julien Bourdeau <julien@sigerr.org>
2017-12-29 11:30:43 +0100

    Add command args to client

--- client/src/buy.go
+++ client/src/buy.go
@@ -15,12 +15,30 @@ type Payload struct {
 }
 
 type Credientials struct {
-	AppId   string `json:"app-id"`
-	ApiKey  string `json:"api-key"`
-	Comment string `json:"comment"`
+  AppId   string `json:"app-id"`
+  ApiKey  string `json:"api-key"`
+  Comment string `json:"comment"`
 }
 
 func main() {
+  if (len(os.Args) != 2) {
+    usage()
+    os.Exit(1)
+  }
+
+  command := os.Args[1]
+
+  if "start" == command {
+    start()
+  } else if "finish" == command {
+    finish()
+  } else {
+    usage()
+  }
+
+}
+
+func start() {
   credentials := getApiKey()
 
   printComment(credentials.Comment)
@@ -61,10 +79,10 @@ func getApiKey() Credientials {
   req.Header.Add("Content-Type", "application/json")
 
   client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("http.Do() error: %v\n", err)
-		os.Exit(100)
+  resp, err := client.Do(req)
+  if err != nil {
+  fmt.Printf("http.Do() error: %v\n", err)
+  os.Exit(100)
   }
   defer resp.Body.Close()
 
@@ -81,4 +99,17 @@ func getApiKey() Credientials {
   }
 
   return credentials
+}
+
+func finish() {
+  printComment("TODO")
+}
+
+func usage() {
+  fmt.Printf("Usage: %s [command]\n", os.Args[0])
+  fmt.Println("Available commands")
+  fmt.Println("\tstart\t\tGet Algolia credentials")
+  fmt.Println("\tfinish\t\tDelete Algolia credentials")
+  fmt.Println("\thelp\t\tPrint this message")
+  fmt.Println()
 }
