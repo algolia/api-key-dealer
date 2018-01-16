a916abe0da91850ec774099cffd44ef8852efda3
Julien Bourdeau <julien@sigerr.org>
2018-01-16 15:35:46 +0100

    Create Places credentials if needed

--- app/Http/Controllers/TravisController.php
+++ app/Http/Controllers/TravisController.php
@@ -37,6 +37,10 @@ class TravisController extends Controller
             $response['mcm'] = $this->getMcmResponse($config, $ip);
         }
 
+        if (in_array('places', $config['want'])) {
+            $response['places'] = $this->getPlacesResponse($config, $ip);
+        }
+
         // Add comment
         $validity = $config['key-params']['validity'] / 60;
         $response['comment'] = "The keys will expire after $validity minutes.";
@@ -46,7 +50,23 @@ class TravisController extends Controller
 
     private function generateKey($appId, $apiKey, $ip, $keyParams)
     {
-        $algolia = new Client($appId, $apiKey);
+        $options = [];
+        $hosts = null;
+
+        // This can be removed when PHP client is updated with
+        // https://github.com/algolia/algoliasearch-client-php/pull/364
+        if ($this->isPlacesApp($appId)) {
+            $options[Client::PLACES_ENABLED] = true;
+            $hosts = array(
+                'places-1.algolianet.com',
+                'places-2.algolianet.com',
+                'places-3.algolianet.com',
+            );
+            shuffle($hosts);
+            array_unshift($hosts, 'places.algolia.net');
+        }
+
+        $algolia = new Client($appId, $apiKey, $hosts, $options);
 
         $response = $algolia->newApiKey($keyParams);
 
@@ -70,5 +90,25 @@ class TravisController extends Controller
             'app-id' => $config['mcm']['app-id'],
             'api-key' => $mcmKey,
         ];
+    }
+
+    private function getPlacesResponse($config, $ip)
+    {
+        $placesKey = $this->generateKey(
+            $config['places']['app-id'],
+            $config['places']['super-admin-key'],
+            $ip,
+            $config['key-params']
+        );
+
+        return [
+            'app-id' => $config['places']['app-id'],
+            'api-key' => $placesKey,
+        ];
+    }
+
+    private function isPlacesApp($appId)
+    {
+        return 'pl' == strtolower(substr($appId, 0, 2));
     }
 }
