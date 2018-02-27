d4f710e4d6c96969b108ed038fd9e610aff1e6b7
Julien Bourdeau <julien@sigerr.org>
2018-02-27 10:28:28 +0100

    Upgrade to Laravel Lumen 5.6

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -2,11 +2,5 @@
 
 return [
     'proxies' => '*',
-    'headers' => [
-        Illuminate\Http\Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-        Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
-        Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-        Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-    ]
+    'headers' => Illuminate\Http\Request::HEADER_X_FORWARDED_ALL,
 ];
