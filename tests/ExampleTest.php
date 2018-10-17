526ede839159b4c793021746fd886ea5c063ea70
Julien Bourdeau <julien@sigerr.org>
2018-10-17 10:52:45 +0200

    Upgrade to Lumen 5.7

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
         $this->get('/');
 
         $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
+            '{"message":"It works!"}', $this->response->getContent()
         );
     }
 }
